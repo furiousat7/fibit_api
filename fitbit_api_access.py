b778b246f437c1b0bdd3fb892b6d9d1142d0fd4f
Aakash <atherani@salesforce.com>
2017-08-03 21:21:50 -0700

    Refactor api to get access token if expired

--- fitbit_api_access.py
+++ fitbit_api_access.py
@@ -9,14 +9,7 @@ client_codes = fitbit_keys.client_codes
 
 oAuthUrl = 'https://api.fitbit.com/oauth2/token'
 
-def checkIfTokenIsValid():
-	# requestBody = {
-	# 	'code': client_codes.get('code'),
-	# 	'redirect_uri': client_codes.get('redirect_uri'),
-	# 	'client_id': client_codes.get('client_id'),
-	# 	'grant_type': 'authorization_code'
-	# }
-	import pdb; pdb.set_trace()	
+def checkIfTokenIsValid(url):
 	requestBody = {
 	 	'grant_type' : 'refresh_token',
         'refresh_token' : tokens.get('refresh_token')
@@ -29,21 +22,37 @@ def checkIfTokenIsValid():
 		request.add_header('Authorization', 'Basic ' + base64.b64encode(client_codes.get('client_id')  +  ":" + client_codes.get('client_secret')))
 		request.add_header('Content-Type', 'application/x-www-form-urlencoded')
 
-		response = urllib2.urlopen(request).read()
-
+		response = urllib2.urlopen(request).read()		
+		import json
+		json_output = json.loads(response)
+		update_tokens(json_output)
+		response = doApiCall(url)
 		print response
 	except urllib2.URLError as e:
 		print e.code
 		print e.read()
 
+def update_tokens(json_output):
+	tokens['access_token'] = json_output.get('access_token')
+	tokens['refresh_token'] = json_output.get('refresh_token')
+	fitbit_keys.tokens = tokens	
+
+
+def doApiCall(url):
+	request = urllib2.Request(url)
+	request.add_header('Authorization', 'Bearer ' +  tokens.get('access_token'))
+	response = urllib2.urlopen(request).read()			
+	return response
+
 if __name__ == '__main__':
 
 	# checkIfTokenIsValid()
-	import pdb; pdb.set_trace()	
 	url = "https://api.fitbit.com/1/user/-/profile.json"
+	try:		
+		doApiCall(url)		
+	except urllib2.URLError as e:
+		HTTPErrorMessage = e.read()
+		if e.code -- 401 and 'Access token expired' in HTTPErrorMessage:
+			checkIfTokenIsValid(url)
 
-	request = urllib2.Request(url)
-	request.add_header('Authorization', 'Bearer ' +  tokens.get('access_token'))
-	response = urllib2.urlopen(request).read()
-	print response
 
