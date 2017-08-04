183dd35205aa5ba35ea2046055b6799b64ee27b5
Aakash <atherani@salesforce.com>
2017-08-03 22:34:50 -0700

    Add handling for refresh token expiry

--- fitbit_api_access.py
+++ fitbit_api_access.py
@@ -11,9 +11,9 @@ oAuthUrl = 'https://api.fitbit.com/oauth2/token'
 
 def checkIfTokenIsValid(url):
 	requestBody = {
-	 	'grant_type' : 'refresh_token',
-        'refresh_token' : tokens.get('refresh_token')
-    }
+		'grant_type' : 'refresh_token',
+		'refresh_token' : tokens.get('refresh_token')
+	}
 
 	try:
 		encodedRequestBody = urllib.urlencode(requestBody)
@@ -22,35 +22,62 @@ def checkIfTokenIsValid(url):
 		request.add_header('Authorization', 'Basic ' + base64.b64encode(client_codes.get('client_id')  +  ":" + client_codes.get('client_secret')))
 		request.add_header('Content-Type', 'application/x-www-form-urlencoded')
 
-		response = urllib2.urlopen(request).read()		
+		response = urllib2.urlopen(request).read()      
 		import json
 		json_output = json.loads(response)
 		update_tokens(json_output)
 		response = doApiCall(url)
 		print response
 	except urllib2.URLError as e:
-		print e.code
-		print e.read()
+		print '-----Refresh token invalid----\n'
+		if e.code == 400 and 'Refresh token invalid' in e.read():
+			getNewRefreshToken(url)
+
+
+def getNewRefreshToken(url):
+	requestBody = {
+		'code' : client_codes.get('code'),
+		'redirect_uri' : client_codes.get('redirect_uri'),
+		'client_id' : client_codes.get('client_id'),
+		'grant_type' : 'authorization_code'
+	}
+	try:
+		import pdb; pdb.set_trace()
+		encodedRequestBody = urllib.urlencode(requestBody)
+		request = urllib2.Request(oAuthUrl, encodedRequestBody)
+		request.add_header('Authorization', 'Basic ' + base64.b64encode(client_codes.get('client_id')  +  ":" + client_codes.get('client_secret')))
+		request.add_header('Content-Type', 'application/x-www-form-urlencoded')
+
+		response = urllib2.urlopen(request).read()
+		import json
+		json_output = json.loads(response)
+		update_tokens(json_output)
+		checkIfTokenIsValid(url)
+	except urllib2.URLError as e:			
+		#this means the code is wrong, manual override
+		print '----manual override----'		
+
 
 def update_tokens(json_output):
 	tokens['access_token'] = json_output.get('access_token')
 	tokens['refresh_token'] = json_output.get('refresh_token')
-	fitbit_keys.tokens = tokens	
+	fitbit_keys.tokens = tokens 
 
 
 def doApiCall(url):
 	request = urllib2.Request(url)
 	request.add_header('Authorization', 'Bearer ' +  tokens.get('access_token'))
-	response = urllib2.urlopen(request).read()			
+	response = urllib2.urlopen(request).read()          
 	return response
 
 if __name__ == '__main__':
 
 	# checkIfTokenIsValid()
 	url = "https://api.fitbit.com/1/user/-/profile.json"
-	try:		
-		doApiCall(url)		
+	try:        
+		doApiCall(url)      
 	except urllib2.URLError as e:
+		print '-----Api call exception----\n'
 		HTTPErrorMessage = e.read()
 		if e.code -- 401 and 'Access token expired' in HTTPErrorMessage:
 			checkIfTokenIsValid(url)
